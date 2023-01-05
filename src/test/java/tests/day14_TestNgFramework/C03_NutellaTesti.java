43a09f531b152a9bb90802c29d331cef3facc79b
Hazel Cebelli <hazelcebel@gmail.com>
2023-01-05 11:20:42 -0800

    1st push on 1st day

---
+++ src/test/java/tests/day14_TestNgFramework/C03_NutellaTesti.java
@@ -0,0 +1,24 @@
+package tests.day14_TestNgFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C03_NutellaTesti {
+    @Test
+    public void test01(){
+        //amazon anasayfaya gidin
+        Driver.getDriver().get("https://www.amazon.com/");
+
+        //nutella icin arama yapin
+        WebElement aramaKutusu=Driver.getDriver().findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+
+        //sonuclarin nutella icerdigini test edin
+        WebElement aramaSonucElemneti=Driver.getDriver().findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+
+
+    }
+}
